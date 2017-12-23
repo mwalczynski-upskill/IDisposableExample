c6ae84dc24b1f3bd7aa3390c1f030c6b7f05ef75
Michal Walczynski <mwalczynski@pgs-soft.com>
2017-12-23 17:07:54 +0100

    Add project files.

---
+++ IDisposableExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IDisposableExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
